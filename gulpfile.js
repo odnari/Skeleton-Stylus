edcf376425898b7244fb250cb73a789f62c4a9e9
Mykola Bodnariuk <rezoh@yandex.ru>
2016-03-07 13:45:51 +0200

    added modules:  - code  - forms  - tables
     updated:
     - variables
     - skeleton.styl
    
     fixed:
     - media queries
    
     added sourcemap generation for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var sourcemaps = require('gulp-sourcemaps');
 
 var skeletonStyl = './stylus/skeleton.styl';
 
 gulp.task('stylus', function () {
     gulp.src(skeletonStyl)
+        .pipe(sourcemaps.init())
         .pipe(stylus({
             compress: true
         }))
+        .pipe(sourcemaps.write('./sourcemap'))
         .pipe(gulp.dest('./css'));
 });
 
